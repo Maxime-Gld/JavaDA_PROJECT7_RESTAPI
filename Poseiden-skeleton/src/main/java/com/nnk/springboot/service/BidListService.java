fa0b1266c345ec54702e9b930b5273b495a3c0d8
Maxime-Gld <Maxime.Gdev@hotmail.com>
2024-09-27 13:20:50 +0200

    maj des interfaces pour correspondre au controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidListService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/BidListService.java
@@ -1,5 +1,19 @@
 package com.nnk.springboot.service;
 
+import java.util.List;
+
+import com.nnk.springboot.domain.BidList;
+
 public interface BidListService {
+
+    void createBidList(BidList bid);
+
+    void deleteBidList(Integer id);
+
+    void updateBidList(Integer id, BidList bid);
+
+    BidList getBidListById(Integer id);
+
+    List<BidList> getAllBidList();
 
 }
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurveService.java
@@ -0,0 +1,18 @@
+package com.nnk.springboot.service;
+
+import java.util.List;
+
+import com.nnk.springboot.domain.CurvePoint;
+
+public interface CurveService {
+
+    void createCurve(CurvePoint curvePoint);
+
+    void updateCurve(int id, CurvePoint curvePoint);
+
+    void deleteCurveById(int curveId);
+
+    List<CurvePoint> getAllCurvePoints();
+
+    CurvePoint getCurveById(Integer id);
+}
