ed7c5251fc857efdd0aca5a04d4ffc4b807f9f53
Maxime-Gld <Maxime.Gdev@hotmail.com>
2024-09-27 13:40:40 +0200

    configuration de spring security

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetailsService.java
@@ -0,0 +1,83 @@
+package com.nnk.springboot.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.nnk.springboot.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+/**
+ * CustomUserDetailsService est une implémentation de l'interface
+ * {@link UserDetailsService}
+ * qui fournit une méthode pour charger les détails d'un utilisateur par son nom
+ * d'utilisateur.
+ * 
+ * Cette classe interagit avec un {@link UserRepository} pour récupérer les
+ * informations
+ * d'utilisateur depuis la base de données et les convertir en un objet
+ * {@link UserDetails},
+ * qui est utilisé par Spring Security pour l'authentification et
+ * l'autorisation.
+ * 
+ * La méthode {@link #loadUserByUsername(String)} est annotée avec
+ * {@link Transactional}
+ * pour s'assurer que les opérations de récupération d'utilisateur soient
+ * exécutées dans
+ * le cadre d'une transaction.
+ * 
+ * Les autorités (rôles) de l'utilisateur sont générées via la méthode privée
+ * {@link #getGrantedAuthorities(String)}, qui retourne une liste de
+ * {@link GrantedAuthority},
+ * incluant le rôle préfixé de "ROLE_".
+ */
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Charge un utilisateur par son nom d'utilisateur et renvoie un objet
+     * {@link UserDetails}.
+     * Si aucun utilisateur n'est trouvé avec le nom d'utilisateur fourni, une
+     * exception
+     * {@link UsernameNotFoundException} est lancée.
+     *
+     * @param username le nom d'utilisateur de l'utilisateur à rechercher
+     * @return un objet {@link UserDetails} contenant les détails de l'utilisateur
+     * @throws UsernameNotFoundException si aucun utilisateur n'est trouvé avec ce
+     *                                   nom d'utilisateur
+     */
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).map(
+                user -> new User(
+                        user.getUsername(),
+                        user.getPassword(),
+                        getGrantedAuthorities(user.getRole())))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    /**
+     * Génère une liste de {@link GrantedAuthority} pour l'utilisateur basé sur son
+     * rôle.
+     * Le rôle est préfixé par "ROLE_" tel qu'attendu par Spring Security.
+     * 
+     * @param role le rôle de l'utilisateur
+     * @return une liste de {@link GrantedAuthority} contenant les rôles de
+     *         l'utilisateur
+     */
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+}
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
@@ -0,0 +1,109 @@
+package com.nnk.springboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * La classe SpringSecurityConfig configure les paramètres de sécurité de
+ * l'application
+ * en utilisant Spring Security.
+ * 
+ * Elle spécifie les règles de gestion des requêtes HTTP, les mécanismes de
+ * login, de logout,
+ * ainsi que la gestion de l'authentification avec un encodeur de mot de passe
+ * {@link BCryptPasswordEncoder}.
+ * 
+ * Cette classe utilise un {@link CustomUserDetailsService} pour charger les
+ * utilisateurs
+ * et leurs rôles depuis une source de données.
+ */
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    /**
+     * Configure la chaîne de filtres de sécurité pour gérer l'autorisation des
+     * requêtes HTTP.
+     * 
+     * Cette méthode désactive la protection CSRF, permet l'accès public à certaines
+     * URL
+     * (telles que les pages de connexion, les pages utilisateur, et les ressources
+     * CSS), et
+     * force l'authentification pour toutes les autres requêtes.
+     * 
+     * Le formulaire de connexion est configuré pour rediriger vers l'URL
+     * "/bidList/list"
+     * après une authentification réussie.
+     * 
+     * @param http l'objet {@link HttpSecurity} permettant de configurer la sécurité
+     *             des requêtes HTTP
+     * @return un objet {@link SecurityFilterChain} représentant la chaîne de
+     *         filtres de sécurité
+     * @throws Exception en cas de configuration incorrecte de la sécurité
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) // Désactivation de la protection CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/user/**", "/css/**") // Autorisation publique
+                                                                     // sur certaines URL
+                        .permitAll()
+                        .anyRequest().authenticated()) // Authentification requise pour toute
+                                                       // autre requête
+                .formLogin(form -> form
+                        .defaultSuccessUrl("/bidList/list", true) // Redirection après succès du
+                                                                  // login
+                        .permitAll())
+                .logout(logout -> logout
+                        .permitAll()) // Autorisation publique pour le logout
+                .build();
+    }
+
+    /**
+     * Crée et configure un encodeur de mot de passe utilisant l'algorithme BCrypt.
+     * 
+     * @return un objet {@link BCryptPasswordEncoder} pour encoder les mots de passe
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * Configure le gestionnaire d'authentification de l'application.
+     * 
+     * Il utilise le {@link CustomUserDetailsService} pour charger les détails des
+     * utilisateurs
+     * et le {@link BCryptPasswordEncoder} pour encoder les mots de passe.
+     * 
+     * @param http                  l'objet {@link HttpSecurity} utilisé pour
+     *                              obtenir les composants partagés
+     * @param bCryptPasswordEncoder l'encodeur de mot de passe utilisé pour vérifier
+     *                              les mots de passe
+     * @return un objet {@link AuthenticationManager} pour gérer l'authentification
+     *         des utilisateurs
+     * @throws Exception en cas d'erreur dans la configuration du gestionnaire
+     *                   d'authentification
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+            BCryptPasswordEncoder bCryptPasswordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+                .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+}
