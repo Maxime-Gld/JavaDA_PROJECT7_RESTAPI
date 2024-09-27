dc7e16c151a167267148ca041da3595e75952616
Maxime-Gld <Maxime.Gdev@hotmail.com>
2024-09-27 13:21:11 +0200

    maj des services pour correspondre au controller

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/impl/BidListServiceImpl.java
@@ -0,0 +1,86 @@
+package com.nnk.springboot.service.impl;
+
+import java.util.List;
+import com.nnk.springboot.domain.BidList;
+import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.service.BidListService;
+
+/**
+ * Implémentation du service pour gérer les opérations CRUD
+ * sur l'entité {@link BidList}.
+ */
+public class BidListServiceImpl implements BidListService {
+
+    private BidListRepository bidListRepository;
+
+    /**
+     * Constructeur avec injection du repository pour la gestion des BidList.
+     * 
+     * @param bidListRepository Le repository utilisé pour les opérations sur les
+     *                          BidList.
+     */
+    public BidListServiceImpl(BidListRepository bidListRepository) {
+        this.bidListRepository = bidListRepository;
+    }
+
+    /**
+     * Crée et enregistre une nouvelle BidList.
+     * 
+     * @param bid La nouvelle BidList.
+     */
+    @Override
+    public void createBidList(BidList bid) {
+        bidListRepository.save(bid);
+    }
+
+    /**
+     * Supprime une BidList par son identifiant.
+     * 
+     * @param id L'identifiant de la BidList à supprimer.
+     */
+    @Override
+    public void deleteBidList(Integer id) {
+        BidList bid = bidListRepository.findById(id).orElse(null);
+        if (bid != null) {
+            bidListRepository.delete(bid);
+        }
+    }
+
+    /**
+     * Met à jour une BidList existante par son identifiant.
+     * 
+     * @param id         L'identifiant de la BidList à mettre à jour.
+     * @param bidUpdated La nouvelle BidList.
+     */
+    @Override
+    public void updateBidList(Integer id, BidList bidUpdated) {
+        BidList bid = bidListRepository.findById(id).orElse(null);
+        if (bid != null) {
+            bid.setAccount(bidUpdated.getAccount());
+            bid.setType(bidUpdated.getType());
+            bid.setBidQuantity(bidUpdated.getBidQuantity());
+            bidListRepository.save(bid);
+        }
+    }
+
+    /**
+     * Récupère une BidList par son identifiant.
+     * 
+     * @param id L'identifiant de la BidList à récupérer.
+     * @return La BidList correspondante, ou null si elle n'existe pas.
+     */
+    @Override
+    public BidList getBidListById(Integer id) {
+        return bidListRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * Récupère toutes les BidList enregistrées.
+     * 
+     * @return La liste de toutes les BidList.
+     */
+    @Override
+    public List<BidList> getAllBidList() {
+        return bidListRepository.findAll();
+    }
+}
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/impl/UserServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/impl/UserServiceImpl.java
@@ -1,47 +1,87 @@
 package com.nnk.springboot.service.impl;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
 import com.nnk.springboot.service.UserService;
 
+/**
+ * Implémentation du service utilisateur qui gère les opérations CRUD
+ * liées à l'entité {@link User}.
+ */
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
 
+    /**
+     * Constructeur avec injection du repository utilisateur.
+     * 
+     * @param userRepository Le repository qui permet d'accéder aux données
+     *                       utilisateur.
+     */
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Crée un nouvel utilisateur et l'enregistre dans la base de données.
+     * 
+     * @param user L'utilisateur à créer.
+     * @return L'utilisateur créé et enregistré.
+     */
     @Override
     public User createUser(User user) {
         return userRepository.save(user);
     }
 
+    /**
+     * Récupère un utilisateur par son identifiant.
+     * 
+     * @param id L'identifiant de l'utilisateur à récupérer.
+     * @return L'utilisateur correspondant à l'identifiant, ou null s'il n'existe
+     *         pas.
+     */
     @Override
     public User getUserById(Integer id) {
         return userRepository.findById(id).orElse(null);
     }
 
+    /**
+     * Récupère la liste de tous les utilisateurs.
+     * 
+     * @return La liste des utilisateurs enregistrés.
+     */
     @Override
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    /**
+     * Met à jour un utilisateur existant.
+     * 
+     * @param id   L'identifiant de l'utilisateur à mettre à jour.
+     * @param user Les nouvelles informations de l'utilisateur.
+     * @return L'utilisateur mis à jour, ou null si l'utilisateur n'existe pas.
+     */
     @Override
-    public User updateUser(Integer id, User user) {
-        User userToUpdate = userRepository.findById(id).orElse(null);
-        if (userToUpdate != null) {
-            userToUpdate.setFullname(user.getFullname());
-            userToUpdate.setRole(user.getRole());
-            userToUpdate.setPassword(user.getPassword());
-            return userRepository.save(userToUpdate);
-        } else {
-            return null;
+    public void updateUser(Integer id, User userUpated) {
+        Optional<User> userToUpdate = userRepository.findById(id);
+        if (userToUpdate.isPresent()) {
+            User userFound = userToUpdate.get();
+            userFound.setFullname(userUpated.getFullname());
+            userFound.setRole(userUpated.getRole());
+            userFound.setPassword(userUpated.getPassword());
+            userRepository.save(userFound);
         }
     }
 
+    /**
+     * Supprime un utilisateur par son identifiant.
+     * 
+     * @param id L'identifiant de l'utilisateur à supprimer.
+     */
     @Override
     public void deleteUser(Integer id) {
         userRepository.deleteById(id);
